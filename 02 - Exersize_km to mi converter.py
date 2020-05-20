6f14acf61aa3370f4e64094097c2c63ca5f8357b
Ben Calvert <benhcalvert@gmail.com>
2020-05-20 17:03:05 -0600

    strings exersize

---
+++ 02 - Exersize_km to mi converter.py
@@ -0,0 +1,5 @@
+print ('This program converts kilometers to miles.')
+kms = float(input('please enter the number of kilometers: '))
+print ('You entered', kms, '. Converting now...')
+miles = kms / 1.609344
+print ('This is equal to ', round(miles, 1), 'miles.')
