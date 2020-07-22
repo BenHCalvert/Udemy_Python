eb52c74c50c65a17558b504c1118304f336e97ab
Ben Calvert <benhcalvert@gmail.com>
2020-07-22 14:46:42 -0600

    BMI calc working. Need to use variable on both sides of the AND oper.

--- bmiCalc.py
+++ bmiCalc.py
@@ -1,5 +1,5 @@
 #ask for user's height and weight. 
-uHeight = float( input('What is your heigh in inches? '))
+uHeight = float( input('What is your height in inches? '))
 uWeight = float( input('What is yout weight in pounds? '))
 
 
@@ -11,11 +11,11 @@ print('Your BMI is: ', round(bmi , 1))
 
 
 # logic tests to determine BMI classification. Both statements with AND must be true for the boolean to return true. if you use OR only one must be true for the test to return TRUE.
-if (bmi > 18.5):
+if (bmi <= 18.5):
     print("Your BMI classification is 'underweight' ")
-elif (bmi > 18.5 and <= 24.9):
+elif (bmi > 18.5 and bmi <= 24.9):
     print("Your BMI classification is 'normal' ")
-elif (bmi > 24.9 and <= 29.9):
+elif (bmi > 24.9 and bmi <= 29.9):
     print("Your BMI classification is 'overweight' ")
 else:
     print("Your BMI classification is 'obese' ")
