209152381754aff9ce859ba1dc751dd5407aae43
Ben Calvert <benhcalvert@gmail.com>
2020-09-30 16:22:46 -0600

    time module

---
+++ time.py
@@ -0,0 +1,16 @@
+import time as t
+time_now = t.localtime()
+
+print ("completed at ", str(time_now.tm_hour) + "H" + str(time_now.tm_min) + "M")
+
+# Arithmatic with t.time to add 7 days to the current_time. 86400 is num of seconds in 1 day.
+current_time = t.time()
+delivery_time = current_time + (86400 * 7)
+print(t.localtime(delivery_time))
+
+t.sleep(5)
+
+
+
+
+
