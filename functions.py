7c1544bb64cc61b148863fe4047e2fbf055be4c5
Ben Calvert <benhcalvert@gmail.com>
2020-09-30 14:52:15 -0600

    addR function

--- functions.py
+++ functions.py
@@ -23,3 +23,8 @@ print (fahr2celcius(100))
 def hiThere(person1, person2="Ben"):
     print("Hello " + person1 + ", how are you doing!?")
 hiThere("Johnny")
+
+def addr(n1=0, n2=0, n3=0, n4=0, n5=0, n6=0, n7=0, n8=0, n9=0, n10=0):
+    sum = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10
+    print("The sum is: ", sum)
+addr(1, 300, 200)
